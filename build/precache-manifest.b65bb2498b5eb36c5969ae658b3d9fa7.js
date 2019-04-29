a575e28471f35f3ae9aa8128215dfdfedd3449db
Sukhareva Maria <maria_suxareva@mail.ru>
2019-04-29 17:55:34 +0300

    Optized images

---
+++ build/precache-manifest.b65bb2498b5eb36c5969ae658b3d9fa7.js
@@ -0,0 +1,154 @@
+self.__precacheManifest = [
+  {
+    "revision": "6754c9c4b01fbaacee24d3dbf5dd8760",
+    "url": "/static/media/GothamPro-Light.6754c9c4.ttf"
+  },
+  {
+    "revision": "b8d11039682b90dd0dff",
+    "url": "/static/css/main.a0cba827.chunk.css"
+  },
+  {
+    "revision": "6708f7e60f48c8a536f5",
+    "url": "/static/js/1.6708f7e6.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "87c940efd574eec947c30f14fbb3b8f7",
+    "url": "/static/media/pesni_logo.87c940ef.png"
+  },
+  {
+    "revision": "55c01eb2ced9d35de375d0f9ebc97005",
+    "url": "/static/media/bottle1.55c01eb2.png"
+  },
+  {
+    "revision": "ecd50d07d7f5c53350ebce836d88dd85",
+    "url": "/static/media/bottle2.ecd50d07.png"
+  },
+  {
+    "revision": "c0a2d2d87d362e298e3227a00f61a2ca",
+    "url": "/static/media/bottle3.c0a2d2d8.png"
+  },
+  {
+    "revision": "d5444bc202edd8325fbfe8ff672cbe7c",
+    "url": "/static/media/bottle4.d5444bc2.png"
+  },
+  {
+    "revision": "f6b6e5047ce647be08e82e710230bcf0",
+    "url": "/static/media/bottle5.f6b6e504.png"
+  },
+  {
+    "revision": "3bdd287278996bd982d5b0b761f1be2f",
+    "url": "/static/media/GothamPro-Black.3bdd2872.ttf"
+  },
+  {
+    "revision": "6222116d2c01b1dfeba3d7970e80849a",
+    "url": "/static/media/GothamPro-Black.6222116d.woff"
+  },
+  {
+    "revision": "33b6cd01c674e0c5d59cde2e48603324",
+    "url": "/static/media/GothamPro-Bold.33b6cd01.eot"
+  },
+  {
+    "revision": "68d644bc10b53bcbd0e09cbaa679fe71",
+    "url": "/static/media/GothamPro-Black.68d644bc.eot"
+  },
+  {
+    "revision": "aaa3ba91a32dda5f2df11d2301764fd8",
+    "url": "/static/media/GothamPro-Bold.aaa3ba91.woff"
+  },
+  {
+    "revision": "57a1bd797d48e05aca3cc4d5a79e07cb",
+    "url": "/static/media/GothamPro-Bold.57a1bd79.ttf"
+  },
+  {
+    "revision": "90f8e664649cf758cd611e68d76e66ab",
+    "url": "/static/media/GothamPro-Light.90f8e664.eot"
+  },
+  {
+    "revision": "caede75f4cdc6d33a14539b2a76d354d",
+    "url": "/static/media/GothamPro-Medium.caede75f.woff"
+  },
+  {
+    "revision": "d24977b3aca5df089137cdc397d4e8d8",
+    "url": "/static/media/GothamPro-Light.d24977b3.woff"
+  },
+  {
+    "revision": "b8d11039682b90dd0dff",
+    "url": "/static/js/main.b8d11039.chunk.js"
+  },
+  {
+    "revision": "cbe1c9c0415b9601b97ea154512615bb",
+    "url": "/static/media/GothamPro-Medium.cbe1c9c0.ttf"
+  },
+  {
+    "revision": "fa67313694dddf4d9526c423fff22510",
+    "url": "/static/media/GothamPro-Medium.fa673136.eot"
+  },
+  {
+    "revision": "698ab18cb9fb73878302342e1c3b1dbd",
+    "url": "/static/media/bottles_eon.698ab18c.png"
+  },
+  {
+    "revision": "08b862847b1dc7530101692972314788",
+    "url": "/static/media/bg3_mob.08b86284.jpg"
+  },
+  {
+    "revision": "2c763890e584df3219b9cfc8954fc9db",
+    "url": "/static/media/firstpage_bg4.2c763890.png"
+  },
+  {
+    "revision": "3f7fa8413a55c48496c74b6ff8167648",
+    "url": "/static/media/icon1.3f7fa841.png"
+  },
+  {
+    "revision": "9c0894e067e589022cf1128dd5f2531c",
+    "url": "/static/media/icon2.9c0894e0.png"
+  },
+  {
+    "revision": "720a77caabde9c36726e4522636263d3",
+    "url": "/static/media/icon3.720a77ca.png"
+  },
+  {
+    "revision": "18b74fcf01a9c63ee298aa40fba70321",
+    "url": "/static/media/icon4.18b74fcf.png"
+  },
+  {
+    "revision": "735717627583cb26c4c4a7007963279d",
+    "url": "/static/media/test_bg.73571762.jpg"
+  },
+  {
+    "revision": "bdec842ff021036c0535374ca83876e3",
+    "url": "/static/media/battery_btn.bdec842f.png"
+  },
+  {
+    "revision": "16d516879840c8c3168b151d6d4073a2",
+    "url": "/static/media/battery1.16d51687.png"
+  },
+  {
+    "revision": "8d1fc60dacb02d19ad644ea195a692c7",
+    "url": "/static/media/battery2.8d1fc60d.png"
+  },
+  {
+    "revision": "98f03e6781a9559deb43bf797be96569",
+    "url": "/static/media/battery3.98f03e67.png"
+  },
+  {
+    "revision": "6a7a85ed59647a9ff77af156f095a121",
+    "url": "/static/media/battery4.6a7a85ed.png"
+  },
+  {
+    "revision": "7a3088d85795257e0329a0346d0e0b97",
+    "url": "/static/media/battery5.7a3088d8.png"
+  },
+  {
+    "revision": "31ecb91facbd113482900fb8130f2c71",
+    "url": "/static/media/about_bg.31ecb91f.jpg"
+  },
+  {
+    "revision": "800c0c8d08ce11df856aafe8b55d8126",
+    "url": "/index.html"
+  }
+];
